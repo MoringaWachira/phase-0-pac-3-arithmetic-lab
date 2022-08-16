5d6e7f6f0370089dcc5910e4c790518b7ee89a24
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-17 00:08:47 +0300

    0

---
+++ index.js
@@ -0,0 +1,80 @@
+function add(a, b) {
+    return a + b;
+}
+add()
+
+
+function subtract(a, b) {
+    return a- b;
+}
+subtract()
+
+
+function multiply(a, b) {
+    return a * b;
+}
+multiply()
+
+function divide(a, b) {
+    return a / b;
+}
+divide()
+
+
+function increment(n) {
+    return (n += 1);
+}
+increment(n);
+
+
+// function add(a, b) {
+//     let result = a + b;
+//     return result;
+// }
+// add(a, b)
+
+
+// function subtract(a, b) {
+//     let result = a - b;
+//     return result;
+// }
+// subtract(a, b)
+
+// function multiply(a, b) {
+//     let result = a * b;
+//     return result;
+// }
+// multiply(a, b)
+
+// function divide(a, b) {
+//     let result = a / b;
+//     return result;
+// }
+// divide(a, b)
+
+
+function decrement(n) {
+    return (n -= 1);
+}
+decrement(n);
+
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+ makeInt()
+
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+
+
+
+
+
+
+
+
